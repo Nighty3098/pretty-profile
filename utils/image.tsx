6514bd09d42d1271c4ce07d24332229278977dca
Nighty3098 <night3098games@gmail.com>
2025-07-27 02:55:42 +0700

    Fix

--- utils/image.tsx
+++ utils/image.tsx
@@ -105,7 +105,7 @@ function renderOverlay(bgUrl: string | undefined) {
           left: 0,
           right: 0,
           bottom: 0,
-          backgroundColor: "rgba(0, 0, 0, 0.1)",
+          backgroundColor: "rgba(0, 0, 0, 0.3)",
           borderRadius: 70,
           zIndex: 1,
         }}
