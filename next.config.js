1204aba7e2eb2075079b30f56bc4bbcf44ba381e
Artem <154594695+Nighty3098@users.noreply.github.com>
2025-12-04 18:08:49 +0700

    Update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,29 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'none'; " +
+              "img-src 'self' https://camo.githubusercontent.com data:; " +
+              "script-src-elem 'sha256-ZswfTY7H35rbv8WC7NXBoiC7WNu86vSzCDChNWwZZDM=' " +
+              "'sha256-yei5Fza+Eyx4G0smvN0xBqEesIKumz6RSyGsU3FJowI='; " +
+              "style-src 'self' 'unsafe-inline';",
+          },
+          {
+            key: "Cache-Control",
+            value: "public, max-age=86400, stale-while-revalidate=86400",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
